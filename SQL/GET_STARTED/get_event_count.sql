51751c906643cef5d9a1c667c77c88dc14682cfc
Christopher Farm <farm.cp@gmail.com>
2016-04-13 17:53:45 -0700

    adding start and end date for counting events

--- SQL/GET_STARTED/get_event_count.sql
+++ SQL/GET_STARTED/get_event_count.sql
@@ -1,6 +1,10 @@
 /*
 * This script allows you to calculate the number of events that you sent to your database
+* @START_DATE => the beginning date you want to start counting your events
+* @END_DATE => the end date you want to end your counting of events
 */
 
 SELECT COUNT(*)
-FROM raw_events;
+FROM raw_events
+WHERE created_at::DATE >= '@START_DATE'
+  AND created_at::DATE < '@END_DATE';
