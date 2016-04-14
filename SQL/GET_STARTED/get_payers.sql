1a5a7deb53b814a2d8d66e3e6e139fcf81cb30ce
Christopher Farm <farm.cp@gmail.com>
2016-04-13 17:23:27 -0700

    adding comments to payers

--- SQL/GET_STARTED/get_payers.sql
+++ SQL/GET_STARTED/get_payers.sql
@@ -1,3 +1,12 @@
+/*
+* This is a simple script to illustrate how easy it is to get a list of advertising_ids that have purchased
+* at least one thing in your app
+* @YOUR_BUNDLE_ID => the bundle id of your app
+* @YOUR_APP_PLATFORM => the platform of your app
+*/
+
 SELECT DISTINCT advertising_id
 FROM raw_events 
-WHERE event_type = 'purchase';
+WHERE event_type = 'purchase'
+  AND bundle_id = '@YOUR_BUNDLE_ID'
+  AND platform = '@YOUR_APP_PLATFORM';
