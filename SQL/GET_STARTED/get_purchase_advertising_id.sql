08b417b2787b47210603ef940732fe535c1b82c9
Makoto <makototaguchi1110@gmail.com>
2016-05-12 00:43:29 -0700

    update get_purchase_advertising_id.sql

--- SQL/GET_STARTED/get_purchase_advertising_id.sql
+++ SQL/GET_STARTED/get_purchase_advertising_id.sql
@@ -5,32 +5,32 @@ revenue => in USD cents
 */
 
 SELECT
-	a.bundle_id
-	, a.platform
-	, a.advertising_id
-	, c.name AS channel
-	, a.acquired_at
-	, a.revenue
+  a.bundle_id
+  , a.platform
+  , a.advertising_id
+  , c.name AS channel
+  , a.acquired_at
+  , a.revenue
 FROM (
-	SELECT
-		bundle_id
-		, platform
-		, advertising_id
-		, source_campaign_id
-		, acquired_at::DATE
-		, SUM(revenue) AS revenue
-	FROM events
-	WHERE
-		acquired_at::DATE >= @DATE
-		AND bundle_id = @BUNDLEID
-		AND event_type = 'purchase'
-	GROUP BY
-		bundle_id
-		, platform
-		, advertising_id
-		, source_campaign_id
-		, acquired_at::DATE
-	HAVING SUM(revenue) > 0
+  SELECT
+    bundle_id
+    , platform
+    , advertising_id
+    , source_campaign_id
+    , acquired_at::DATE
+    , SUM(revenue) AS revenue
+  FROM events
+  WHERE
+    acquired_at::DATE >= @DATE
+    AND bundle_id = @BUNDLEID
+    AND event_type = 'purchase'
+  GROUP BY
+    bundle_id
+    , platform
+    , advertising_id
+    , source_campaign_id
+    , acquired_at::DATE
+  HAVING SUM(revenue) > 0
 ) a
 LEFT OUTER JOIN campaigns b
 ON a.source_campaign_id = b.id
