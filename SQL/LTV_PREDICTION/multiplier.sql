09c308a5a673f3daf94eedd5542f9cb0ec9618a6
Makoto <makototaguchi1110@gmail.com>
2016-05-21 01:25:08 -0700

    add multiplier.sql

---
+++ SQL/LTV_PREDICTION/multiplier.sql
@@ -0,0 +1,52 @@
+/* 
+* This is the query to generate 14, 30, 60, 90days ltv multiplier against 7days ltv for each channel.
+* Once you figure out the multiplier, you can predict x-day ltv in each cohort 
+* by multipling actual 7days ltv by the multiplier.
+*
+* Replace these variables before running the query. 
+* @DATE => refers to the date that you want to get the multiplier. This has to be >= 90days before the current date
+* @BUNDLEID => bundle_id for your app
+*/
+
+SELECT
+  a.bundle_id
+  , a.platform
+  , c.name AS channel
+  , SUM(a.rev_14d)/SUM(a.rev_7d) :: DOUBLE PRECISION AS multiplier_14d_7d
+  , SUM(a.rev_30d)/SUM(a.rev_7d) :: DOUBLE PRECISION AS multiplier_30d_7d
+  , SUM(a.rev_60d)/SUM(a.rev_7d) :: DOUBLE PRECISION AS multiplier_60d_7d
+  , SUM(a.rev_90d)/SUM(a.rev_7d) :: DOUBLE PRECISION AS multiplier_90d_7d
+FROM (
+  SELECT
+    bundle_id
+    , platform
+    , source_campaign_id
+    , SUM(CASE WHEN date_diff('sec', acquired_at, created_at) / 86400 <= 7
+    THEN revenue END) AS rev_7d
+    , SUM(CASE WHEN date_diff('sec', acquired_at, created_at) / 86400 <= 14
+    THEN revenue END) AS rev_14d
+    , SUM(CASE WHEN date_diff('sec', acquired_at, created_at) / 86400 <= 30
+    THEN revenue END) AS rev_30d
+    , SUM(CASE WHEN date_diff('sec', acquired_at, created_at) / 86400 <= 60
+    THEN revenue END) AS rev_60d
+    , SUM(CASE WHEN date_diff('sec', acquired_at, created_at) / 86400 <= 90
+    THEN revenue END) AS rev_90d
+  FROM events
+  WHERE
+    acquired_at::DATE <= '@DATE'
+    AND bundle_id = '@BUNDLEID'
+    AND event_type = 'purchase'
+  GROUP BY
+    bundle_id
+    , platform
+    , source_campaign_id
+) a
+LEFT OUTER JOIN campaigns b
+ON a.source_campaign_id = b.id
+LEFT OUTER JOIN ad_networks c
+ON b.ad_network_id = c.id
+GROUP BY 
+  a.bundle_id
+  , a.platform
+  , c.name
+;
