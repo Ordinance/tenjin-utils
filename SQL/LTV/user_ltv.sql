831a09b49f68d546fa96796d424e2af67f6d8cec
Christopher Farm <farm.cp@gmail.com>
2016-11-15 22:38:50 -0800

    update ltv and add per user ltv

--- SQL/LTV/user_ltv.sql
+++ SQL/LTV/user_ltv.sql
@@ -1,4 +1,4 @@
-/* Calculate the cohorted LTV for payers up to 120-days after the install date. 
+/* Calculate the cohorted LTV for users up to 120-days after the install date. 
  * Use date_diff to figure out when to sum up revenue values.  
  * Filter for event_type = purchase to to limit events to payers only.
  * @BUNDLEID is the bundle_id or package name of your app that you're calculating LTV per payer for
@@ -7,7 +7,7 @@
 
 SELECT 
   acquired_at::date,
-  count(distinct advertising_id) AS "Purchasers",
+  count(distinct advertising_id) AS "Installs",
   sum(CASE
         WHEN date_diff('sec',acquired_at,created_at)/86400 <= 7 then revenue/100.0
       END)/count(distinct advertising_id) AS "7-day",
@@ -25,7 +25,6 @@ SELECT
       END)/count(distinct advertising_id) AS "120-day"
 FROM events
 WHERE bundle_id = '@BUNDLEID'
-  AND event_type = 'purchase'
   AND acquired_at <= '2016-11-14'
 GROUP BY 1
 ORDER BY 1 ASC;
